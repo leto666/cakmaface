0f8b892b9668062d800ee533c59744efdbe8b37c
Kutlu Ata <32374530+leto666@users.noreply.github.com>
2019-12-06 22:06:45 +0300

    Add files via upload

--- face.js
+++ face.js
@@ -2,6 +2,13 @@ var database = [
 	{
 		userName: "kutlu",
 		password: "123"
+	},
+	{	userName: "mirna",
+		password: "123"
+	},
+	{
+		userName:"yasmin",
+		password:"123"
 	}
 ];
 
@@ -20,25 +27,39 @@ var newsfeed = [
 	}
 ];
 
-var userNameSor = prompt("adınnız nedir?");
-var sifreSor = prompt("sifreniz nedir?");
+var userNameSor = prompt("Adiniz nedir?");
+var sifreSor = prompt("Sifreniz nedir?");
 
-function sifreDogrulama(a,b) {
-	if (a===database[0].userName && b===database[0].password ){
+userOK=false;
+function usercheck(k,j) {
+for (var i=0; i<database.length; i++) {
+	if (database[i].userName===k && database[i].password===j ) {
+	 	userOK = true;
+	 	} 
+}
+}
+
+usercheck(userNameSor,sifreSor);
+
+
+if (userOK===true ) {
+	alert("Giris basarili!");
 		var kim = prompt("hosgeldin, kimi merak ettin?");
 			if (kim === "Ersan") {
-		document.getElementById("kimdemis").innerHTML = newsfeed[0].username;
-		document.getElementById("nedemis").innerHTML = newsfeed[0].timeline;
+		document.getElementById("kimdemis").innerHTML = "Arkadaşınız  " + newsfeed[0].username;
+		document.getElementById("nedemis").innerHTML = newsfeed[0].timeline + "  demiş";
 		} else if (kim === "Omer") {
-		document.getElementById("kimdemis").innerHTML = newsfeed[1].username;
-		document.getElementById("nedemis").innerHTML = newsfeed[1].timeline;
+		document.getElementById("kimdemis").innerHTML = "Arkadaşınız  " + newsfeed[1].username;
+		document.getElementById("nedemis").innerHTML = newsfeed[1].timeline + "  demiş";
+		} else if (kim === "Mirkelam") {
+		document.getElementById("kimdemis").innerHTML = "Arkadaşınız  " + newsfeed[2].username;
+		document.getElementById("nedemis").innerHTML = newsfeed[2].timeline + "  demiş";
 		} else {
-		document.getElementById("kimdemis").innerHTML = newsfeed[2].username;
-		document.getElementById("nedemis").innerHTML = newsfeed[2].timeline;
+			document.getElementById("hic").innerHTML = "Böyle bir arkadaşınız yok!!!";
 		}
 
 
-	} else { alert("tekrar deneyin");}
-}
+	} else { alert("Adiniz yada sifreniz hatali!");}
 
-sifreDogrulama(userNameSor,sifreSor);
+	
+
